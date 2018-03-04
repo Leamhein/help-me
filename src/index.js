81963c870e18cdef291b7f3cefefe92d60a7205a
Vladimir Apenko <Leamhein@hotmail.com>
2018-03-05 00:09:34 +0300

    Speed up factorised N

--- src/index.js
+++ src/index.js
@@ -2,36 +2,55 @@ module.exports = function count(s, pairs) {
   var N = 1,
   b = 1,
   counter = 0,
-  ans = 0;
-  function evclid (b) {
-    var a = N;
-    while (a && b) {
-      if (a >= b) {
-        a %= b;
-      } else {
-        b %= a;
-      }
+  ans = 0, //answer
+  euler, //
+  simple = new Set; //set of simple multiply
+
+  function Factorise (base) { //factorise function
+    var j = 1,
+    i = 2; // divider
+    if (base == i) {
+      simple.add(2);
     };
-    if (a | b == 1) {
-      return true;
+    do {
+     if (base % i == 0) { // base
+      simple.add(i);
+      j++;
+      base = base / i;
+    } else {
+      i++;
+      };
+    } while (i <= base);
+    return simple; //return max simple multiplier
     };
-    return false;
-  };
+
+  function Euler (N) { //Euler function
+      for (let key of simple) {
+        euler *= (1 - 1 / key);
+        };
+      return Math.round(euler);
+    };
+
   for (let i = 0, length = pairs.length; i < length; i++) {
-    N *= pairs[i][0] * pairs[i][1];
-    console.log("N= "+ N);
+    N *= pairs[i][0] * pairs[i][1]; //calc N
   };
-  for (let i = 1; i < N; i++) {
-    counter += evclid (b + i);
+
+  euler = N;
+
+  for (let i = 0, length = pairs.length; i < length; i++) {
+    Factorise(pairs[i][0]); //factorise every elements in pairs array
+    Factorise(pairs[i][1]);
   };
-  console.log("counter= "+ counter);
+
+  Euler(N);
+
   for (let i = 0, length = s.length; i < length; i++) {
     if (s[i] == "1") {
-      ans += counter;
+      ans += euler;
     } else {
-      ans += (N - counter);
-    }
-  }
+      ans += N - euler;
+    };
+  };
   ans %= 1000000007;
   return ans;
 };
