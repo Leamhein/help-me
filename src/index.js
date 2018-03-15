3690246d9e4bdc52096d440a1b8208ca29f3bf70
Vladimir Apenko <Leamhein@hotmail.com>
2018-03-15 20:57:30 +0300

    New logic for s.length > 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function count(s, pairs) {
-  let N = 1,
-  simple,
-  iSimple,
-  ans = 0;
+  let ans = 0,
+  N = 1,
+  simple = new Set, // set of simple multipliers
+  corConditional = [], //array of multipliers that satisfy the condition s[0]
+  repeating = []; //array of repeating multipliers
 
   function Factorise (base) { //factorise function
-    let simple = new Set,
-    j = 1,
+    let j = 1,
     i = 2; // divider
     if (base == i) {
       simple.add(2);
@@ -23,7 +23,7 @@ module.exports = function count(s, pairs) {
     return simple;
   };
 
-  function Euler (N, simple) { //Euler function
+  function Euler (N) { //Euler function
     var euler = N;
     for (let key of simple) {
       if (key > N) {
@@ -34,34 +34,68 @@ module.exports = function count(s, pairs) {
     return Math.floor(euler);
   };
 
+  function Euclid (a, b) { //Euclid algorithm
+    while (a != 0 && b != 0) {
+      if (a > b) {
+        a %= b;
+      } else {
+        b %= a;
+      };
+    };
+    return a + b;
+  };
+
   for (let i = 0, length = pairs.length; i < length; i++) {
     N *= Math.pow(pairs[i][0], pairs[i][1]); //calc N
-    if (N > 3000000000000) {
-      return NaN;
+    Factorise(pairs[i][0]); //factorise N
+  };
+  if (N > 100000000) {
+    return NaN;
+  };
+
+  if (s.length == 1) {
+    if (s[0] == "1") {
+      ans += Euler(N);
+    } else {
+      ans += N - Euler(N);
+    };
+    return ans %= 1000000007;
+  };
+
+for (let i = 0, length = s.length; i < length; i++) {
+  if (s[i] == "1" && i == 0) {
+    for (let j = i; j < N + i; j++) {
+      if (Euclid(N, j) == 1) {
+        corConditional.push(j-i); // if this is the first condition => push all satisfied multipliers
+      };
     };
   };
 
-  simple = Factorise(N);
+  if (s[i] == "0" && i == 0) {
+    for (let j = i; j < N + i; j++) {
+      if (Euclid(N, j) != 1) {
+        corConditional.push(j-i); // if this is the first condition => push all satisfied multipliers
+      };
+    };
+  };
 
-  for (let i = 0, length = pairs.length; i < length; i++) {
-    if (s[i] == "1") {
-      switch (i) {
-        case 0: ans += Euler(N, simple);
-          break;
-        case i > 0: iSimple = Factorise(i);
-          ans += Euler(N, simple) - Euler(i, iSimple);
-          break;
-      }
-    } else {
-      switch (i) {
-        case 0: ans += N - Euler(N, simple);
-          break;
-        case i > 0: iSimple = Factorise(i);
-          ans += N - Euler(N, simple) - (i - Euler(i, iSimple));
-          break;
-      }
-    }
-  }
-ans %= 1000000007;
-return ans;
+  if (s[i] == "1" && i != 0) {
+    for (let j = i; j < N + i; j++) {
+      if (Euclid(N, j) == 1 && corConditional.includes(j-i)) {
+        repeating.push(j-i); //push only repeating multipliers
+      };
+    };
+  };
+
+  if (s[i] == "0" && i != 0) {
+    for (let j = i; j < N + i; j++) {
+      if (Euclid(N, j) != 1 && corConditional.includes(j-i)) {
+        repeating.push(j-i); //push only repeating multipliers
+      };
+    };
+  };
+
+};
+
+return repeating.length % 1000000007;
 };
